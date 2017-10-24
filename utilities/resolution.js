45713b06ef54af20dfd3150041fdc5a352825f81
nattatorn.dev <nattatorn.dev@gmail.com>
2017-10-24 15:55:23 +0700

    Add aspectRatioStandardScreen, aspectRatioWideScreen func to resolution

--- utilities/resolution.js
+++ utilities/resolution.js
@@ -1,8 +1,26 @@
-import { Dimensions, Platform } from 'react-native'
+import { Dimensions, PixelRatio, Platform } from 'react-native'
 
 const isIos = Platform.OS === 'ios'
 const isIphoneX = isIos && Dimensions.get( 'window' ).height === 812
 
 const baseNavMarginTop = () => ( isIphoneX ? 22 + 14 : 22 )
 
-export { baseNavMarginTop, isIos, isIphoneX }
+const aspectRatio = ( horizontal, vertical ) => {
+  return {
+    height: PixelRatio.roundToNearestPixel(
+      Dimensions.get( 'window' ).width / ( horizontal / vertical )
+    ),
+  }
+}
+
+const aspectRatioStandardScreen = () => aspectRatio( 4, 3 )
+const aspectRatioWideScreen = () => aspectRatio( 16, 9 )
+
+export {
+  aspectRatio,
+  aspectRatioStandardScreen,
+  aspectRatioWideScreen,
+  baseNavMarginTop,
+  isIos,
+  isIphoneX,
+}
